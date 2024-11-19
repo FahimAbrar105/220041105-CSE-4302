8a317fd3139e5b4787a0ccfbc406976093cbc95d
Fahim Abrar <fahimabrar22@iut-dhaka.edu>
2024-11-19 20:14:44 +0600

    Added Lab9

---
+++ Lab 9/220041105_T01L09_1A.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Artwork
+{
+protected:
+    string title;
+    string name;
+    int price;
+
+public:
+    Artwork(string t, string n, int p) : title(t), name(n), price(p) {}
+    virtual void displayDetails()
+    {
+        cout << "Title: " << title << endl;
+        cout << "Artist: " << name << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+class Painting : public Artwork
+{
+protected:
+    string medium;
+
+public:
+    Painting(string t, string n, int p, string m) : Artwork(t, n, p), medium(m) {}
+    void displayDetails()
+    {
+        Artwork::displayDetails();
+        cout << "Medium: " << medium << endl;
+    }
+};
+
+class Sculpture : public Artwork
+{
+protected:
+    string material;
+
+public:
+    Sculpture(string t, string n, int p, string m) : Artwork(t, n, p), material(m) {}
+    void displayDetails()
+    {
+        Artwork::displayDetails();
+        cout << "Material: " << material << endl;
+    }
+};
+
+class DigitalArt : public Artwork
+{
+protected:
+    string resolution;
+
+public:
+    DigitalArt(string t, string n, int p, string r) : Artwork(t, n, p), resolution(r) {}
+    void displayDetails()
+    {
+        Artwork::displayDetails();
+        cout << "Resolution: " << resolution << endl;
+    }
+};
+
+int main()
+{
+    Artwork *arts[5];
+    arts[0] = new Painting("Painting 1", "Person 1", 100000, "Acrylic");
+    arts[1] = new Sculpture("Painting 2", "Person 2", 200000, "Bronze");
+    arts[2] = new DigitalArt("Painting 3", "Person 3", 300000, "800x600");
+    arts[3] = new Painting("Painting 4", "Person 4", 400000, "Waterpaint");
+    arts[4] = new Sculpture("Painting 5", "Person 5", 500000, "Ash");
+
+    for (int i = 0; i < 5; i++)
+    {
+        arts[i]->displayDetails();
+        cout << endl;
+    }
+}
